e5c3c9f4b968891f00be2b29a60f431dea55e0c3
Agustin Baza <agustinbaza10@gmail.com>
2023-06-05 01:23:08 -0300

    finalice el ejercicio, falta resposive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Maquetado from './components/Maquetado';
-
+import React, { useState, useEffect } from "react";
+import Maquetado from "./components/Maquetado";
 
 function App() {
+  const [categoria, setCategoria] = useState("general");
+  const [noticias, setNoticias] = useState([]);
 
-  
+  useEffect(() => {
+    const obtenerNoticias = async () => {
+      try {
+        const apiKey = "c38f9e4109d64ccf9cb1348ca22f400e";
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${categoria}&apiKey=${apiKey}`;
+        const respuesta = await fetch(url);
+        const datos = await respuesta.json();
+        setNoticias(datos.articles);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    obtenerNoticias();
+  }, [categoria]);
+
+  const handleConsultarAPI = (categoriaSeleccionada) => {
+    setCategoria(categoriaSeleccionada);
+  };
+
   return (
-  <Maquetado></Maquetado>
-
-  )
+    <>
+      <Maquetado consultarAPI={handleConsultarAPI} noticias={noticias} />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Maquetado.jsx
+++ src/components/Maquetado.jsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
-import { Dropdown} from "react-bootstrap";
+import React from "react";
+import "./Maquetado.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Noticias from "./Noticias";
 
-function Maquetado() {
+const Maquetado = ({ consultarAPI, noticias }) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const categoriaSeleccionada = event.target.categoria.value;
+    consultarAPI(categoriaSeleccionada);
+  };
 
-    return (
-        <section>
-        <h1 className='text-center mt-4'> Noticias </h1>
-        <hr />
-        <article className='orden'>
-        <div className='d-flex justify-content-around'>
-          <h4 className='mt-4 p-4'>Buscar por categoria: </h4> 
-          <Dropdown className="mt-4 p-4">
-      <Dropdown.Toggle variant="dark" id="dropdown-basic">
-        Categorias
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item value="top"> Ultimo momento </Dropdown.Item>
-        <Dropdown.Item value="sports"> Deportes </Dropdown.Item>
-        <Dropdown.Item value="entertainment"> Entretenimiento</Dropdown.Item>
-        <Dropdown.Item value="technology">Tecnologia</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+  return (
+    <section>
+      <h1 className="text-center mt-4">Noticias</h1>
+      <hr />
+      <article className="orden">
+        <div className="d-flex justify-content-evenly align-items-center mt-5 mb-5">
+          <h2 className="">Buscar por categoría:</h2>
+          <div className="ordenBoton">
+            <form onSubmit={handleSubmit}>
+              <select className="input" name="categoria">
+                <option value="general">General</option>
+                <option value="sports">Deportes</option>
+                <option value="entertainment">Entretenimiento</option>
+                <option value="technology">Tecnología</option>
+              </select>
+              <button type="submit" className="btn btn-dark">
+                Buscar
+              </button>
+            </form>
+          </div>
         </div>
         <hr />
-        </article>
-        
-       </section>
-    )
-}
+        <Noticias noticias={noticias} />
+      </article>
+    </section>
+  );
+};
 
-export default Maquetado
-
+export default Maquetado;
